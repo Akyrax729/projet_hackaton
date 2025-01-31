2849ec566b9d100d05a50ad8423df53667d6852c
Ben <ben24france@gmail.com>
2025-01-31 11:17:44 +0100

    responsive cassé à 980px, mais globalement fait

--- src/Form/ContactType.php
+++ src/Form/ContactType.php
@@ -5,8 +5,8 @@ namespace App\Form;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
-use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\Extension\Core\Type\EmailType;
+use Symfony\Component\Form\Extension\Core\Type\TextareaType;
 
 class ContactType extends AbstractType
 {
@@ -15,7 +15,7 @@ class ContactType extends AbstractType
         $builder
             ->add('email', EmailType::class)
             ->add('name')
-            ->add('message', TextType::class)
+            ->add('message', TextareaType::class)
         ;
     }
 
